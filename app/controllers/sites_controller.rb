21775683e54e96d880821ff6d80ef1f89f0d067c
striking <chris@strikinggroup.com.au>
2016-05-30 18:50:24 +1000

    added site CRUD controls

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,16 +4,45 @@ class SitesController < ApplicationController
     @sites = Site.all
   end
 
+  def show
+    @site = Site.find(params[:id])
+  end
+
   def new
     @site = Site.new
   end
 
   def create
-    @site = Site.new
+    @site = Site.new(site_params)
     if @site.save
-      redirect_to sites_path
+      redirect_to @site
     else
       render :new
     end
+  end
+
+  def edit
+    @site = Site.find(params[:id])
+  end
+
+  def update
+    @site = Site.find(params[:id])
+    if @site.update(site_params)
+      redirect_to @site
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @site = Site.find(params[:id])
+    @site.destroy
+      redirect_to sites_path
+  end
+
+  private
+
+  def site_params
+    params.require(:site).permit(:title, :address, :suburb)
   end
 end
