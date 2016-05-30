d2930e20ed68a3046fd9d5ec061de9a81fa88083
striking <chris@strikinggroup.com.au>
2016-05-30 10:26:56 +1000

    added bootstrap and simple form for

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,2 +1,19 @@
 class SitesController < ApplicationController
+
+  def index
+    @sites = Site.all
+  end
+
+  def new
+    @site = Site.new
+  end
+
+  def create
+    @site = Site.new
+    if @site.save
+      redirect_to sites_path
+    else
+      render :new
+    end
+  end
 end
